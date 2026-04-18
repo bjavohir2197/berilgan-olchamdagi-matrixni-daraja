abc5b45a80db418f755ef08193a930586e739b91
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 12:49:35 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Berilgan N x N o‘lchamdagi matrixni 90 daraja aylantiring (soat yo‘nalishi bo‘yicha).")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
